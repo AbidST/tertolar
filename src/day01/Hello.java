859bc8210fccf4a3fd75392a6b9b10fffd304b0b
Abid <i_learn_tek@outlook.com>
2022-04-08 15:05:52 +0300

    Hello print

--- src/day01/Hello.java
+++ src/day01/Hello.java
@@ -1,4 +1,10 @@
 package day01;
 
 public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("New");
+    }
+
 }
--- src/day01/Local.java
+++ src/day01/Local.java
@@ -3,6 +3,8 @@ package day01;
 public class Local {
     public static void main(String[] args) {
         System.out.println("Local");
+
+        System.out.println("Step1");
     }
 
 
